7c2dceb79b5a3a24bb0b5ff912f990f7cf0eb3b4
someone <someone@example.com>
2021-12-06 15:54:50 +0800

    c

--- promote/01_clear_repo.sh
+++ promote/01_clear_repo.sh
@@ -2,14 +2,14 @@
 
 FROM=app1-docker-dev-local
 curl -H 'X-JFrog-Art-Api: '"$ART_API_KEY"'' \
--X DELETE "http://182.92.214.141:8082/artifactory/$FROM"
+-X DELETE "https://soleng.jfrog.io/artifactory/$FROM"
 
 
 FROM=app1-docker-test-local
 curl -H 'X-JFrog-Art-Api: '"$ART_API_KEY"'' \
--X DELETE "http://182.92.214.141:8082/artifactory/$FROM"
+-X DELETE "https://soleng.jfrog.io/artifactory/$FROM"
 
 
 FROM=app1-docker-prod-local
 curl -H 'X-JFrog-Art-Api: '"$ART_API_KEY"'' \
--X DELETE "http://182.92.214.141:8082/artifactory/$FROM"
+-X DELETE "https://soleng.jfrog.io/artifactory/$FROM"
